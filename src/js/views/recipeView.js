64830c3752c72ba4143464a2270ab2d3fa1ede30
Sejeedz <m.sajid3421@gmail.com>
2022-11-13 00:31:26 +0500

    ADDED README

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View';
 // import icons from '../img/icons.svg' // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-const Fraction = require('fracty');
+const fraction = require('fracty');
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -105,7 +105,7 @@ class RecipeView extends View {
     <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      ing.quantity ? fraction(ing.quantity).toString() : ''
     }</div>
     <div class="recipe__description">
     <span class="recipe__unit">${ing.unit}</span>

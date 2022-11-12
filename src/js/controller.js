d595201e5ae4407a5532202e81abfdefcd2c7064
Sejeedz <m.sajid3421@gmail.com>
2022-11-13 00:06:53 +0500

    TEST

--- src/js/controller.js
+++ src/js/controller.js
@@ -116,6 +116,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('easy scene');
 };
 
 init();

6e3f6645f3b2ce99e681e574430370352fea36b1
Sejeedz <m.sajid3421@gmail.com>
2022-11-12 23:40:38 +0500

    added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -108,6 +108,9 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -116,6 +119,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 
 init();
